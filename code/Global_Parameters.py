3eec8f84a3182a0bbbfe22b31e65fe81950b31e6
zvibinyamin <zvibinyamin@gmail.com>
2016-05-10 10:47:27 +0300

    Update Global_Parameters.py

--- code/Global_Parameters.py
+++ code/Global_Parameters.py
@@ -1,1 +1,9 @@
-public static final String CONST_NAME = "Name";
+class Parameters_const():
+    def Max_Battery(self):
+        return 50
+    def Transmission_range(self):
+        return 500
+    def Robots_not_move(self):
+        return 10
+    def Robots_move(self):
+        return 90
