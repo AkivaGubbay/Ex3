97ddb69b7c28ff93f92eb2cb0cb2e4dd58d8ed56
zvibinyamin <zvibinyamin@gmail.com>
2016-05-12 22:12:30 +0300

    Update Main.py

--- code/Main.py
+++ code/Main.py
@@ -1,5 +1,8 @@
 from code.Point import Point
+from code.Zone import Zone
 from code.Robot import Robot
+from code.Global_Parameters import *
+
 
 
 #Test Point:
@@ -7,10 +10,16 @@ print('Test Point:')
 p = Point(4,3)
 print(p.toString())
 
-
 #Test Robot:
 print('Test Robot:')
 r = Robot(12)
 print('initial battery: '+str(r.battery_status))
 r.messages.append('Joey: How you doing..')
 print(r.messages.pop())
+
+#Test Zone:
+print('Test Zone:')
+z=Zone()
+z.PrintRobot_By_XY()
+#z.PrintType_by_XY()
+
