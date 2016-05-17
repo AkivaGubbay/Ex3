d11ec5336f99e4f789be757237658e6785f3336d
akiva <gubbayakiva@gmail.com>
2016-05-17 15:55:14 +0300

    updated robot

--- code/Robot.py
+++ code/Robot.py
@@ -1,17 +1,28 @@
+from code.Point import *
 from code.Global_Parameters import *
 
-# TO-DO:
-#Add Field so that robot knows what area he is in(white,black,gray).
+
+
 class Robot:
 
     def __init__(self, id):
-        self.id = id
-        self.battery_status = BATTARY_CAPACITY()
-        self.messages = []           #All received messages
-        self.isTransmitting = True      #Robot is either transmitting or receiving
-        self.neighbors_list = []
-        self.CanMove = True
+        self._id = id
+        self._can_move = True
+        self._battery_status = BATTARY_CAPACITY()
+        self._private_location = Point(0,0)
+        self._real_location = Point(0,0)
+        self._message_log = []          #All received messages
+        self._private_location_log = [] #Holds all the robots movements as list of points
+        self._neighbors_color = []      #Can save all his neighbors colors
+        self._neighbors_list = []
+        self._out_book = []             #Messages waiting to be sent.
+        self._time = -1                 #Robots always knows the time.
+        self._currently_sending = -1
+        self._currently_get_message = -1
+        self._current_zone = -1
+        self._distance_from = []#I want to fill the whole list with -1 but i dont knot how many neighbors..
 
+    """
     def haveMessage(self, message): #differant versions..
         return message in self.messages
 
@@ -20,9 +31,9 @@ class Robot:
         self.neighbors_list.append(neighbor)
 
     def hasNeighbor(self, neighbor):
-        return neighbor in self.neighbors_list
+        return neighbor in self.neighbors_list    """
 
     def toString(self):
-        return "ID:"+str(self.id)+" ,battery_status:"+str(self.battery_status)+" ,messages:"+str(self.messages)+" ,isTransmitting:"+str(self.isTransmitting)+" ,neighbors_list:"+str(self.neighbors_list)+" ,CanMove:"+str(self.CanMove)
+        return "id:"+str(self._id)+" , battery status:"+str(self._battery_status)+" , message log:"+str(self._message_log)+" ,neighbors list:"+str(self._neighbors_list)+" ,can move:"+str(self._can_move)
 
 
